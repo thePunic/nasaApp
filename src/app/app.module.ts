b04f6e524a83c39990a3895590317f5d7dd786fd
thePunic <127594581+thePunic@users.noreply.github.com>
2023-03-12 02:59:45 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import {MatButtonModule} from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
 import { AppComponent } from './app.component';
 import {MatInputModule} from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -26,12 +23,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatButtonModule,
     HttpClientModule,
     MatInputModule,
-    MatSelectModule,    
-    BrowserAnimationsModule,
     MatFormFieldModule,    
     MatRadioModule,
     MatCardModule,   
-    MatDatepickerModule,
     FormsModule,
     ReactiveFormsModule,
     MatNativeDateModule
